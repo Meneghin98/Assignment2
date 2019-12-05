f49305d6bc4ff98ea721428d69c88ca2cb2a8a6d
Simone Meneghin <simone.meneghin.98@outlook.it>
2019-12-05 16:02:50 +0100

    Corretto checkstyle

--- sandwich-shop-manager/src/main/java/it/unipd/tos/Bill.java
+++ sandwich-shop-manager/src/main/java/it/unipd/tos/Bill.java
@@ -1,3 +1,7 @@
+////////////////////////////////////////////////////////////////////
+// Simone Meneghin 1174926
+////////////////////////////////////////////////////////////////////
+
 package it.unipd.tos;
 
 import it.unipd.tos.business.TakeAwayBill;
@@ -10,14 +14,17 @@ import java.util.List;
 public class Bill implements TakeAwayBill {
     
     @Override
-    public double getOrderPrice(List<MenuItem> itemsOrdered) throws TakeAwayBillException {
+    public double getOrderPrice(List<MenuItem> itemsOrdered)
+            throws TakeAwayBillException {
         double conto = 0.0;
-        if (itemsOrdered.isEmpty())
+        if (itemsOrdered.isEmpty()) {
             return conto;
+        }
         
-        if (itemsOrdered.size()>30)
+        if (itemsOrdered.size()>30) {
             throw new TakeAwayBillException();
-    
+        }
+        
         for (MenuItem item : itemsOrdered) {
             conto += item.getPrice();
         }
@@ -31,18 +38,20 @@ public class Bill implements TakeAwayBill {
         double min = 99999999999.0;
         if (panini.size()>=5){
             for (MenuItem panino: panini) {
-                if (panino.getPrice() < min)
+                if (panino.getPrice() < min) {
                     min = panino.getPrice();
+                }
             }
             conto -= min/2;
         }
     
-        if (conto > 50.0)
+        if (conto > 50.0) {
             return conto *= 0.9;
-    
-        if (conto <10)
+        }
+        
+        if (conto <10) {
             return conto += 0.5;
-    
+        }
     
         return conto;
     }
