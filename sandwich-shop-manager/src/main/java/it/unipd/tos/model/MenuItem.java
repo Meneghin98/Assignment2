3a57b0b7e8c7daa2ed2b02ae3a201c13179f308d
Simone Meneghin <simone.meneghin.98@outlook.it>
2019-12-05 14:52:02 +0100

    Impostati tests

--- sandwich-shop-manager/src/main/java/it/unipd/tos/model/MenuItem.java
+++ sandwich-shop-manager/src/main/java/it/unipd/tos/model/MenuItem.java
@@ -5,4 +5,15 @@
 package it.unipd.tos.model;
 
 public class MenuItem {
+    public enum ItemType {panini, fritti, bevande};
+    
+    private ItemType type;
+    private String name;
+    private double price;
+    
+    public MenuItem(ItemType t, String n, double p){
+        type=t;
+        name=n;
+        price=p;
+    }
 }
---
+++ sandwich-shop-manager/src/test/java/it/unipd/tos/TakeAwayBillTest.java
@@ -0,0 +1,108 @@
+////////////////////////////////////////////////////////////////////
+// Simone Meneghin 1174926
+// //////////////////////////////////////////////////////////////////
+
+package it.unipd.tos;
+
+import it.unipd.tos.business.TakeAwayBill;
+import it.unipd.tos.business.exception.TakeAwayBillException;
+import it.unipd.tos.model.MenuItem;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Unit test for simple App.
+ */
+public class TakeAwayBillTest
+    extends TestCase
+{
+    
+    TakeAwayBill takeAwayBill;
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public TakeAwayBillTest(String testName )
+    {
+        super( testName );
+        takeAwayBill = new Bill();
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( TakeAwayBillTest.class );
+    }
+    
+    /**
+     * Test c
+     * @throws Exception
+     */
+    @org.junit.Test
+    public void TestRisultatoCorretto() throws TakeAwayBillException {
+    
+        List<MenuItem> ordini = new ArrayList<>();
+        ordini.add(new MenuItem(MenuItem.ItemType.bevande, "Coca", 2.5));
+        ordini.add(new MenuItem(MenuItem.ItemType.panini, "Primavera", 3.7));
+        ordini.add(new MenuItem(MenuItem.ItemType.fritti, "Olive", 3));
+        ordini.add(new MenuItem(MenuItem.ItemType.panini, "Veg", 3.2));
+        ordini.add(new MenuItem(MenuItem.ItemType.bevande, "Fanta", 2));
+        
+        double conto = takeAwayBill.getOrderPrice(ordini);
+        assertEquals(14.4, conto);
+    }
+    
+    @org.junit.Test
+    public void Test5Panini() throws TakeAwayBillException {
+        List<MenuItem> ordini = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            ordini.add(new MenuItem(MenuItem.ItemType.panini, "panino", 2.0));
+        }
+        
+        assertEquals(11, takeAwayBill.getOrderPrice(ordini));
+    }
+    
+    @org.junit.Test
+    public void TestScontoTot() throws TakeAwayBillException {
+        List<MenuItem> ordini = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            ordini.add(new MenuItem(MenuItem.ItemType.bevande, "cola", 3.0));
+        }
+        
+        assertEquals(54.0, takeAwayBill.getOrderPrice(ordini));
+    }
+    
+    @org.junit.Test(expected = TakeAwayBillException.class)
+    public void TestGt30Elementi() throws TakeAwayBillException {
+        List<MenuItem> ordini = new ArrayList<>();
+        for (int i = 0; i < 31; i++) {
+            ordini.add(new MenuItem(MenuItem.ItemType.fritti, "olive", 2.5));
+        }
+        
+        takeAwayBill.getOrderPrice(ordini);
+    }
+    
+    @org.junit.Test
+    public void TestMinEuro10() throws TakeAwayBillException {
+        List<MenuItem> ordini = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            ordini.add(new MenuItem(MenuItem.ItemType.bevande, "Cola", 2.0));
+        }
+        assertEquals(8.5, takeAwayBill.getOrderPrice(ordini));
+    }
+    
+    @org.junit.Test
+    public void TestListaVuota() throws TakeAwayBillException {
+        List<MenuItem> ordini = new ArrayList<>();
+        takeAwayBill.getOrderPrice(ordini);
+    }
+    
+
+}
